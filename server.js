180482ee010d32746264067cc364b0b86f665cc5
Feda Zidan <fido311@gmail.com>
2020-10-26 19:30:07 -0500

    Fixed eslint again

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /* eslint-disable linebreak-style */
-const express = require("express");
+var express = require("express");
 
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
-const app = express();
+var app = express();
 
 
 app.use(express.static("public"));
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
