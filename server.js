d103db396a0fd4c88a932ea9bc2ebcdf9bf2844f
Kalena Almand <kalmand1057@gmail.com>
2020-10-26 19:15:30 -0500

    no changes were made

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 const express = require("express");
 
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -8,16 +9,16 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(routes);
 
 app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+    console.log("App now listening at localhost:" + PORT);
 });
