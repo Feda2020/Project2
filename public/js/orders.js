3f7a6b7ec569e555ce2254de57c2349f4dabd111
Lisa Munoz <lisa_munoz817@yahoo.com>
2020-10-31 14:04:14 -0500

    Added modal on createOrder and orders page

--- public/js/orders.js
+++ public/js/orders.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
   $(".createOwn").on( "click", function() {
     window.location.replace("/createorder");
   });
+  $(".add-pizza").on("click", function() {
+    $("#checkOutModal").modal("show");
+    const name = $(this)[0].parentElement.children[0].textContent;
+    const firstTop = $(this)[0].parentElement.children[1].textContent;
+    const secondTop = $(this)[0].parentElement.children[2].textContent;
+    const thirdTop = $(this)[0].parentElement.children[3].textContent;
+    $(".pizza-name").text(name);
+    $(".one").text(firstTop);
+    $(".two").text(secondTop);
+    $(".three").text(thirdTop);
+  });
 });
