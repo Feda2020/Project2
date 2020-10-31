46d2f7abfc143fbf775ce3a6a8930ae081a1c3f8
Kalena Almand <kalmand1057@gmail.com>
2020-10-31 14:17:57 -0500

    deleted checkout fav and reviews js from models and created checkout and favs api calls

--- models/checkout.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    let Checkout = sequelize.define("checkout", {
-    //   id: {
-    //     type: DataTypes.INTEGER,
-    //     allowNull: true
-    //   }
-    });
-    return Checkout;
-  };
--- models/favorites.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    let Favorites = sequelize.define("favorites", {
-    //id: {
-    //     type: DataTypes.INTEGER,
-    //     allowNull: true
-    //   }
-    });
-    return Favorites;
-  };
