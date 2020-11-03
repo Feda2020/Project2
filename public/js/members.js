4e5cf75eea1701d6faa7318d70400a83e93d618f
Drew A <alvarez.drew@gmail.com>
2020-11-03 15:07:20 -0600

    cleaned up more css and got previous orders ability to reorder

--- public/js/members.js
+++ public/js/members.js
@@ -3,20 +3,37 @@ $(document).ready(function() {
     $(".member-name").text(data.email);
   });
 
-  function previousOrders() {
-    $.post("/api/presetorders", {
-      pizzaName: pizzaName,
-      toppingOne: toppingOne,
-      toppingTwo: toppingTwo,
-      toppingThree: toppingThree
-    })
-      .then(function(response) {
-      });
-  }
-
   $("#order-here").on( "click", function() {
     window.location.replace("/order");
   });
 
-  previousOrders();
+  $(".add-pizza").on("click", function() {
+    $("#checkOutModal").modal("show");
+    const name = $(this)[0].parentElement.children[0].textContent;
+    const firstTop = $(this)[0].parentElement.children[1].textContent;
+    const secondTop = $(this)[0].parentElement.children[2].textContent;
+    const thirdTop = $(this)[0].parentElement.children[3].textContent;
+    $(".pizza-name").text(name);
+    $(".one").text(firstTop);
+    $(".two").text(secondTop);
+    $(".three").text(thirdTop);
+  });
+
+  $(".submitOrder").on("click", function(event){
+    event.preventDefault();
+    $.get("/api/user_data").then(function(data) {
+      let userId = data.id;
+      let newOrder = {
+        pizzaName: $(".pizza-name").text(),
+        toppingOne: $(".one").text(),
+        toppingTwo: $(".two").text(),
+        toppingThree: $(".three").text(),
+        saveById: userId
+      };
+      $.post("/api/orders", newOrder, function(){
+        window.location.replace("/checkout");
+      });
+    });
+
+  });
 });
